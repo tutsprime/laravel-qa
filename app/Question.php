35889c5bd533836dd7f6c2d43f3cd5037e84e4f6
TutsPrime <tutsprimedotcom@gmail.com>
2018-05-25 10:41:50 +0700

    Add votes, answers and views counter to question item

--- app/Question.php
+++ app/Question.php
@@ -27,4 +27,15 @@ class Question extends Model
     {
         return $this->created_at->diffForHumans();
     }
+
+    public function getStatusAttribute()
+    {
+        if ($this->answers > 0) {
+            if ($this->best_answer_id) {
+                return "answered-accepted";
+            }
+            return "answered";
+        }
+        return "unanswered";
+    }
 }
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -10,6 +10,17 @@
                 <div class="card-body">
                    @foreach ($questions as $question)
                         <div class="media">
+                            <div class="d-flex flex-column counters">
+                                <div class="vote">
+                                    <strong>{{ $question->votes }}</strong> {{ str_plural('vote', $question->votes) }}
+                                </div>                            
+                                <div class="status {{ $question->status }}">
+                                    <strong>{{ $question->answers }}</strong> {{ str_plural('answer', $question->answers) }}
+                                </div>                            
+                                <div class="view">
+                                    {{ $question->views . " " . str_plural('view', $question->views) }}
+                                </div>                            
+                            </div>
                             <div class="media-body">
                                 <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
                                 <p class="lead">
