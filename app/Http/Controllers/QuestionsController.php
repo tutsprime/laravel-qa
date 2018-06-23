05bb8e78621ef11e4b148e5eaddb497d40730d52
TutsPrime <tutsprimedotcom@gmail.com>
2018-06-24 00:32:28 +0700

    Implement delete functionality for the question

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -89,6 +89,8 @@ class QuestionsController extends Controller
      */
     public function destroy(Question $question)
     {
-        //
+        $question->delete();
+
+        return redirect('/questions')->with('success', "Your question has been deleted.");
     }
 }
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -36,6 +36,11 @@
                                     <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
                                     <div class="ml-auto">
                                         <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
+                                        <form class="form-delete" method="post" action="{{ route('questions.destroy', $question->id) }}">
+                                            @method('DELETE')
+                                            @csrf
+                                            <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">Delete</button>
+                                        </form>
                                     </div>
                                 </div>
                                 <p class="lead">
