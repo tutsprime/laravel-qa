97f507e770b31156025276e66506f810f1ddc3f1
TutsPrime <tutsprimedotcom@gmail.com>
2018-06-30 02:31:49 +0700

    Fix Collection could not be converted to int error

--- app/Policies/QuestionPolicy.php
+++ app/Policies/QuestionPolicy.php
@@ -31,6 +31,6 @@ class QuestionPolicy
      */
     public function delete(User $user, Question $question)
     {
-        return $user->id === $question->user_id && $question->answers < 1;
+        return $user->id === $question->user_id && $question->answers_count < 1;
     }
 }
