2442d33ba2efadf5745c31da5c24782c2ea33ac6
TutsPrime <tutsprimedotcom@gmail.com>
2018-07-02 01:24:37 +0700

    Update best_answer_id on questions table when an answer removed using Eloquent event

--- app/Answer.php
+++ app/Answer.php
@@ -32,12 +32,22 @@ class Answer extends Model
         });        
 
         static::deleted(function ($answer) {
-            $answer->question->decrement('answers_count');
+            $question = $answer->question;
+            $question->decrement('answers_count');
+            if ($question->best_answer_id === $answer->id) {
+                $question->best_answer_id = NULL;
+                $question->save();
+            }
         });
     }
 
     public function getCreatedDateAttribute()
     {
         return $this->created_at->diffForHumans();
+    }
+
+    public function getStatusAttribute()
+    {
+        return $this->id === $this->question->best_answer_id ? 'vote-accepted' : '';
     }
 }
--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -18,7 +18,7 @@
                             <a title="This answer is not useful" class="vote-down off">
                                 <i class="fas fa-caret-down fa-3x"></i>
                             </a>
-                            <a title="Mark this answer as best answer" class="vote-accepted mt-2">
+                            <a title="Mark this answer as best answer" class="{{ $answer->status }} mt-2">
                                 <i class="fas fa-check fa-2x"></i>                                    
                             </a>
                         </div>
