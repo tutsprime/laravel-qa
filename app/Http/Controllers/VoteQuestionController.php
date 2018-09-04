8079c2b615c2c976909f7af56ddb72f2aec96e6d
TutsPrime <tutsprimedotcom@gmail.com>
2018-09-05 00:24:49 +0700

    Provide json response on VoteQuestionController

--- app/Http/Controllers/VoteQuestionController.php
+++ app/Http/Controllers/VoteQuestionController.php
@@ -15,7 +15,14 @@ class VoteQuestionController extends Controller
     {
         $vote = (int) request()->vote;
 
-        auth()->user()->voteQuestion($question, $vote);
+        $votesCount = auth()->user()->voteQuestion($question, $vote);
+
+        if (request()->expectsJson()) {
+            return response()->json([
+                'message' => 'Thanks for the feedback',
+                'votesCount' => $votesCount
+            ]);
+        }
 
         return back();
     }
