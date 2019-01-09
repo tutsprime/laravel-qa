30f16e68bb7b3ec791c9ded4220a529df6b47d9a
TutsPrime <tutsprimedotcom@gmail.com>
2019-01-09 12:35:15 +0700

    Force https in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
     }
 
     /**
