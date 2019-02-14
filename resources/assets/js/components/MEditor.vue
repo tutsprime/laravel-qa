48231aa84edf6bb53139d1c9b0f62f4502c9a5d0
TutsPrime <tutsprimedotcom@gmail.com>
2019-02-14 11:40:28 +0700

    make the preview tab functional

--- resources/assets/js/components/MEditor.vue
+++ resources/assets/js/components/MEditor.vue
@@ -14,13 +14,22 @@
             <div class="tab-pane active" id="write">
                 <slot></slot>
             </div>
-            <div class="tab-pane" id="preview">Preview..</div>
+            <div class="tab-pane" v-html="preview" id="preview"></div>
         </div>
     </div>
 </template>
 
 <script>
+import MarkdownIt from 'markdown-it';
+const md = new MarkdownIt();
+
 export default {
-    props: ['body']
+    props: ['body'],
+
+    computed: {
+        preview () {
+            return md.render(this.body);
+        }
+    }
 }
 </script>
