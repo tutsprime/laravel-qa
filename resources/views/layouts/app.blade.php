98d273b050c347b3c0548e684f18a9297fe2cb71
TutsPrime <tutsprimedotcom@gmail.com>
2019-07-03 20:15:50 +0700

    Modify the Navbar's text

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,9 +22,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
+                <a class="navbar-brand" href="{{ url('/') }}">Laravel Q/A</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
