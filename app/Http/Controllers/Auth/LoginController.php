0e26bb6d0a3cc431aad69b86e9da3cad252de415
TutsPrime <tutsprimedotcom@gmail.com>
2019-09-28 10:56:45 +0700

    Create Api endpoints for Authentication

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -38,20 +38,4 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
-
-    public function getToken(Request $request)
-    {
-        $request->request->add([
-            'grant_type' => 'password',
-            'client_id' => 2,
-            'client_secret' => 'Ubvt8PmMHXkgXBHE3hYeWTVrpFuOJSwpkB280uCo',
-            'username' => $request->username,
-            'password' => $request->password,
-        ]);
-
-        $requestToken = Request::create(env('APP_URL') . '/oauth/token', 'post');
-        $response = Route::dispatch($requestToken);
-
-        return $response;
-    }
 }
--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::post('/token', 'Auth\LoginController@getToken');
+Route::post('/login', 'Api\Auth\LoginController@store');
+Route::delete('/logout', 'Api\Auth\LoginController@destroy')->middleware('auth:api');
+Route::post('/register','Api\Auth\RegisterController');
+
 Route::get('/questions', 'Api\QuestionsController@index');
 Route::get('/questions/{question}/answers', 'Api\AnswersController@index');
 Route::get('/questions/{question}-{slug}', 'Api\QuestionDetailsController');
